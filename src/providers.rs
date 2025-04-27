bb49744c45d1c06f3b0ccb342a810b05c570a802
Andy Méthion <andymethion@gmail.com>
2025-04-27 12:42:04 +0200

    feat: get_block(), get_transactions(), get_localized_transaction_traces()

--- src/providers.rs
+++ src/providers.rs
@@ -1,16 +1,18 @@
-use std::{env, str::FromStr};
+use std::{env, str::FromStr, vec::IntoIter};
 
 use alloy::{
-    eips::BlockId,
     network::Ethereum,
-    providers::{
-        fillers::{BlobGasFiller, ChainIdFiller, FillProvider, GasFiller, JoinFill, NonceFiller},
-        Identity, Provider, ProviderBuilder, RootProvider, WsConnect,
-    },
+    primitives::{Address, TxHash, B256},
     pubsub::SubscriptionStream,
-    rpc::types::{Block, BlockTransactionsKind, Header},
+    rpc::types::{Block, BlockTransactionsKind, Filter, Header, Log, Transaction},
     transports::http::reqwest::Url,
 };
+use alloy_provider::{
+    ext::TraceApi,
+    fillers::{BlobGasFiller, ChainIdFiller, FillProvider, GasFiller, JoinFill, NonceFiller},
+    Identity, Provider, ProviderBuilder, RootProvider, WsConnect,
+};
+use alloy_rpc_types_trace::parity::LocalizedTransactionTrace;
 
 type WsProvider = FillProvider<
     JoinFill<
@@ -31,18 +33,11 @@ type HttpProvider = FillProvider<
 >;
 
 pub struct Providers {
-    pub ws_provider: WsProvider,
+    ws_provider: WsProvider,
     pub http_provider: HttpProvider,
 }
 
 impl Providers {
-    pub async fn new() -> Self {
-        return Providers {
-            ws_provider: Providers::get_ws_provider().await,
-            http_provider: Providers::get_http_provider(),
-        };
-    }
-
     async fn get_ws_provider() -> WsProvider {
         let ws_url = env::var("WS_URL").unwrap();
         let ws_connect = WsConnect::new(ws_url);
@@ -51,8 +46,15 @@ impl Providers {
 
     fn get_http_provider() -> HttpProvider {
         let http_url = env::var("HTTP_URL").unwrap();
-        let url: Url = Url::from_str(&http_url).unwrap();
+        let url = Url::from_str(&http_url).unwrap();
         return ProviderBuilder::new().on_http(url);
+    }
+
+    pub async fn new() -> Self {
+        return Self {
+            ws_provider: Self::get_ws_provider().await,
+            http_provider: Self::get_http_provider(),
+        };
     }
 
     pub async fn get_stream(&self) -> SubscriptionStream<Header> {
@@ -60,12 +62,43 @@ impl Providers {
         return subscription.into_stream();
     }
 
-    pub async fn get_block(&self, block_number: u64) -> Block {
+    async fn get_block(&self, block_number: u64) -> Block {
         return self
             .http_provider
-            .get_block(BlockId::number(block_number), BlockTransactionsKind::Full)
+            .get_block(block_number.into(), BlockTransactionsKind::Full)
             .await
             .unwrap()
             .unwrap();
+    }
+
+    pub async fn get_transactions(&self, block_number: u64) -> IntoIter<Transaction> {
+        let block = self.get_block(block_number).await;
+        return block.transactions.into_transactions();
+    }
+
+    pub async fn get_localized_transaction_traces(
+        &self,
+        transaction_hash: TxHash,
+    ) -> Vec<LocalizedTransactionTrace> {
+        return self
+            .http_provider
+            .trace_transaction(transaction_hash)
+            .await
+            .unwrap();
+    }
+
+    pub async fn get_logs(&self, block_hash: B256) -> Vec<Log> {
+        let filter = Filter::new().at_block_hash(block_hash);
+        return self.http_provider.get_logs(&filter).await.unwrap();
+    }
+
+    pub async fn get_transaction_from(&self, transaction_hash: TxHash) -> Address {
+        return self
+            .http_provider
+            .get_transaction_by_hash(transaction_hash)
+            .await
+            .unwrap()
+            .unwrap()
+            .from;
     }
 }
