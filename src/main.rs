e4c200e2c4d6e79e93be5507899ec69f23633a53
Andy Méthion <andymethion@gmail.com>
2025-04-27 13:06:33 +0200

    feat: pools_handler, swaps_handler, transfers_handler

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 use futures_util::StreamExt;
+use pools_handler::PoolsHandler;
 use providers::Providers;
+use swaps_handler::SwapsHandler;
 use tokens_handler::TokensHandler;
+use tokio::join;
+use transfers_handler::TransfersHandler;
 
+mod pools_handler;
 mod providers;
+mod swaps_handler;
 mod tokens_handler;
+mod transfers_handler;
 
 #[tokio::main]
 async fn main() {
@@ -13,11 +20,25 @@ async fn main() {
     let mut stream = providers.get_stream().await;
 
     let tokens_handler = TokensHandler::new(&providers);
+    let transfers_handler = TransfersHandler::new();
+    let pools_handler = PoolsHandler::new();
+    let swaps_handler = SwapsHandler::new();
 
     while let Some(header) = stream.next().await {
-        let block = providers.get_block(header.number).await;
-        let transactions = block.transactions.into_transactions();
-        tokens_handler.handle(transactions).await;
-        println!("Block Number {} Handled", block.header.number);
+        println!("Block Number {} Received", header.number);
+
+        let (transactions, logs) = join!(
+            providers.get_transactions(header.number),
+            providers.get_logs(header.hash),
+        );
+
+        tokens_handler.handle(transactions.clone()).await;
+        transfers_handler.handle(&logs);
+        pools_handler.handle(&logs);
+        swaps_handler.handle(&logs);
+
+        println!("Transactions Len: {}", transactions.len());
+        println!("Logs Len: {}", logs.len());
+        println!("Block Number {} Handled", header.number);
     }
 }
